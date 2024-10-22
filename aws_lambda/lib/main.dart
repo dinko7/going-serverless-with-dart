85c40179cd8c47b9e76236a6d6c4e39ca278f1cd
Dinko Marinac <dinkomarinac7@gmail.com>
2024-10-22 21:29:45 +0200

    Dynamo DB example.

--- aws_lambda/lib/main.dart
+++ aws_lambda/lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:aws_lambda/functions.dart' as fn;
+import 'package:aws_lambda/functions.dart';
 import 'package:aws_lambda_dart_runtime_ns/aws_lambda_dart_runtime_ns.dart';
 
 Future<void> main(List<String> args) async {
-  await invokeAwsLambdaRuntime([
-    fn.sayHelloWorld,
-  ]);
+  await invokeAwsLambdaRuntime([handleTodoCreation]);
 }
